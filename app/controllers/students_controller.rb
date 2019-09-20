f25cce235dcfcc322390d93f3e1d1bb58131fd16
Angus Morrison <contact@angus-morrison.com>
2019-09-20 11:17:40 +0100

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,11 +12,8 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create(
-      first_name: params[:student][:first_name],
-      last_name: params[:student][:last_name]
-    )
-    render new_student_path
+    session[:student] = params[:student]
+    redirect_to new_student_path
   end
 
 end
